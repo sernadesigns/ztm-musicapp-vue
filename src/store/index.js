59e353b38b13f94fcd02ebdc4b91752aa3ee2b16
sernadesigns <sernadesigns@gmail.com>
2021-06-09 14:16:18 -0600

    Initial app creation, breaks out header and authModal components

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,18 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    authModalShow: false,
   },
   mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
+  },
+  getters: {
+    // getters are like computed for state
+    // computeds are good for computations
+    // using a getter for returning state (as is) is overkill
+    // authModalShow: (state) => state.authModalShow,
   },
   actions: {
   },
